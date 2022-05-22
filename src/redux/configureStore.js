6f3b4467f6e48d681ad1da78efed1b954d623077
Ahmed Sayed <ahmed.hassanein@wadidegla.com>
2022-05-22 22:44:37 +0200

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,19 @@
-import { createStore } from "redux";
-import { Reducer ,initState} from "./reducer";
+import { combineReducers, createStore } from "redux";
+import { Comments } from "./comments";
+import { Dishes } from "./dishes";
+import { Leaders } from "./leaders";
+import { Promotions } from "./promotions";
 
 export const ConfigureStore=()=>{
     const store = createStore(
-        Reducer, // reducer
-        initState, // our initialState
+        combineReducers({
+            dishes:Dishes,
+            comments:Comments,
+            promotions:Promotions,
+            leaders:Leaders
+
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
     return store;
